54951b36186612aa1bc9ccc7703ddf05c0a0bebc
Marlene <31359415+MNB-dev@users.noreply.github.com>
2021-11-16 13:46:31 -0300

    Fix

--- SistemaLaboratorio/src/views/PeticionesYPracticas.java
+++ SistemaLaboratorio/src/views/PeticionesYPracticas.java
@@ -16,38 +16,33 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.text.DefaultCaret;
 import java.awt.Window.Type;
 
-public class PeticionesYPracticas extends JFrame {
+public class PeticionesYPracticas extends JDialog {
 
-	private final JFrame frame;
-
-	/**
-	 * Create the frame.
-	 */
 	public PeticionesYPracticas(List<PeticionDTO> l) throws Exception {
-		frame = new JFrame("Peticiones y prácticas");
-		frame.getContentPane().setBackground(Color.WHITE);
-		frame.setType(Type.POPUP);
-		frame.setBackground(Color.WHITE);
-		frame.getContentPane().setLayout(new GridLayout(0, 1));
-		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		frame.setSize(1100, 500);
-		frame.setLocationRelativeTo(null);
-		frame.setVisible(true);
+		this.setTitle("Peticiones y prácticas");
+		this.getContentPane().setBackground(Color.WHITE);
+		this.setType(Type.POPUP);
+		this.setBackground(Color.WHITE);
+		this.getContentPane().setLayout(new GridLayout(0, 1));
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		this.setSize(900, 500);
+		//this.setLocationRelativeTo();
+		this.setVisible(true);
 
 		JLabel lblPracticasAsoc = new JLabel("PETICIONES Y PRACTICAS ASOCIADAS");
 		lblPracticasAsoc.setBackground(Color.WHITE);
 		lblPracticasAsoc.setFont(new Font("Tahoma", Font.BOLD, 18));
 		lblPracticasAsoc.setForeground(new Color(200, 32, 58));
-		frame.getContentPane().add(lblPracticasAsoc);
+		this.getContentPane().add(lblPracticasAsoc);
 
 		for(int i = 0; i < l.size(); i++) {
 			PeticionDTO p = l.get(i);
 			JLabel lbl = new JLabel("Petición ID: " + p.id + " | Sucursal asociada: " + p.sucursalID + " | Estado de petición: " + p.estadoPeticion);
 			lbl.setFont(new Font("Tahoma", Font.BOLD, 13));
-			frame.getContentPane().add(lbl);
+			this.getContentPane().add(lbl);
 			agregarPracticasAsociadas(p.practicasAsociadas);
-			frame.validate();
-			frame.repaint();
+			this.validate();
+			this.repaint();
 		}
 	}
 
@@ -55,7 +50,6 @@ public class PeticionesYPracticas extends JFrame {
 	private void agregarPracticasAsociadas(List<PracticaAsociadaDTO> list) throws Exception {
 		for (final PracticaAsociadaDTO p : list) {
 			JLabel lblpracticaAsocidada = new JLabel();
-			//JLabel lblpractica = new JLabel();
 			lblpracticaAsocidada.setFont(new Font("Tahoma", Font.PLAIN, 12));
 			lblpracticaAsocidada.setForeground(Color.darkGray);
 
@@ -67,12 +61,12 @@ public class PeticionesYPracticas extends JFrame {
 				txtPractica.setFont(new Font("Tahoma", Font.PLAIN, 11));
 				//txtPractica.setText(" Nombre: " + practica.nombre + "\n Grupo: " + practica.grupo + "\n Valores criticos" + "\n MAX: " + practica.valorCriticoMax + " MIN: " + practica.valorReservadoMin + "\n Valores Reservados" + "\n MAX: " + practica.valorReservadoMin + " MIN: " + practica.valorReservadoMax + "\n Horas de espera: " + practica.horasEsperaResultado + "\n Estado de la practica: " + practica.estadoPractica);
 				txtPractica.setText(" Nombre: " + practica.nombre + " | Grupo: " + practica.grupo + " | Valores criticos: MAX: " + practica.valorCriticoMax + " MIN: " + practica.valorReservadoMin + " | Valores Reservados: MAX: " + practica.valorReservadoMin + " MIN: " + practica.valorReservadoMax + " | Horas de espera: " + practica.horasEsperaResultado + " | Estado de la practica: " + practica.estadoPractica);
-				frame.getContentPane().add(lblpracticaAsocidada);
-				frame.getContentPane().add(txtPractica);
+				this.getContentPane().add(lblpracticaAsocidada);
+				this.getContentPane().add(txtPractica);
 			}
 			else {
 				lblpracticaAsocidada.setText("> Practica ID: " + p.practicaID + " Resultado: RETIRAR POR SUCURSAL");
-				frame.getContentPane().add(lblpracticaAsocidada);
+				this.getContentPane().add(lblpracticaAsocidada);
 			}
 		}
 	}
